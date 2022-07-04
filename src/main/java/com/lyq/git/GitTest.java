04a9905dc5073df0b3bf654de66a88a357559630
LuYaQiang <1684172633@qq.com>
2022-07-04 17:53:05 +0800

    first commit

--- src/main/java/com/lyq/git/GitTest.java
+++ src/main/java/com/lyq/git/GitTest.java
@@ -3,5 +3,6 @@ package com.lyq.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2");
     }
 }
